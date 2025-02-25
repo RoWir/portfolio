0d480e3cb9fd70d031c0934ca71c8df8f6da6866
Robin Wirth <wirth-robin@web.de>
2025-02-25 23:50:44 +0100

    ls

--- src/components/terminal/FileSystemContext.tsx
+++ src/components/terminal/FileSystemContext.tsx
@@ -1,9 +1,10 @@
-import React, { createContext, PropsWithChildren, SetStateAction, useEffect, useState } from "react";
+import React, { createContext, PropsWithChildren, useState } from "react";
 
 // Create a context to provide `setCommandPrefix`
 export type FileSystemContextType = {
     changeNode: (path: string) => boolean,
-    currentPath: string[]
+    currentPath: string[],
+    createFile: (fileName: string) => boolean
 }|null;
 
 export const FileSystemContext = createContext<FileSystemContextType>(null);
@@ -26,6 +27,15 @@ export const CommandPrefixProvider: React.FC<PropsWithChildren> = ({ children })
     const [systemTree, setSystemTree] = useState<TreeNode>([{type:'folder' as const,name:'test',children:[{type:'folder' as const,name:'test2',children:[]}]}]);
     const [currentPath, setCurrentPath] = useState<string[]>([]);
 
+    const getCurrentNode = () => {
+        let currentNode: TreeNode = systemTree;
+        currentPath.forEach(path => {
+            const folder = systemTree.find(node => node.type === 'folder' && node.name === path);
+            if (folder && folder.type === 'folder') currentNode = folder.children;
+        });
+        return currentNode;
+    }
+
     const changeNode = (path:string) => {
         const pathArray = path.split("/").filter(Boolean);
 
@@ -38,13 +48,8 @@ export const CommandPrefixProvider: React.FC<PropsWithChildren> = ({ children })
                 return true;
             }
         }
-
-        let currentNode: TreeNode = systemTree;
-        currentPath.forEach(path => {
-            const folder = systemTree.find(node => node.type === 'folder' && node.name === path);
-            if (folder && folder.type === 'folder') currentNode = folder.children;
-        })
-
+        let currentNode = getCurrentNode();
+        
         for (const path of pathArray) {
             const folder = currentNode.find(node => node.type === 'folder' && node.name === path);
             if (folder && folder.type === 'folder') {
@@ -57,8 +62,21 @@ export const CommandPrefixProvider: React.FC<PropsWithChildren> = ({ children })
         return true;
     }
 
+    const createFile = (fileName:string) => {
+        const currentNode = getCurrentNode();
+
+        const fileNameTaken = currentNode.find(node => node.name.trim() === fileName.trim());
+
+        if (fileNameTaken) {
+            return false;
+        } else {
+            
+            return true;
+        }
+    }
+
     return (
-    <FileSystemContext.Provider value={{ changeNode, currentPath }}>
+    <FileSystemContext.Provider value={{ changeNode, currentPath, createFile }}>
         {children}
     </FileSystemContext.Provider>
     );
---
+++ src/components/terminal/terminalfunction/ls.tsx
@@ -0,0 +1,12 @@
+import { TerminalFunction } from "./_types";
+
+const Ls:TerminalFunction = () => {
+    
+    return ""
+}
+
+export default Ls;
+
+export function description() {
+    return "Zeigt Inhalte des aktuellen Verzeichnises an"
+}
